dabf94bcdd0e51baad9ac107c83a5a2b19ee94fd
Ouassila00 <145725226+Ouassila00@users.noreply.github.com>
2025-04-14 14:27:21 +0200

    basis code voor header toegevoegd

--- showcase-portfolio/app/layout.js
+++ showcase-portfolio/app/layout.js
@@ -1,4 +1,4 @@
-
+import Header from "./Components/Header/Header";
 import "./globals.css";
 
 
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
       </body>
     </html>
